5e631bf91e744aabc3e53b8030b7e9bcf6dce1ff
borared <bora.red@student.cadt.edu.kh>
2025-10-14 14:54:25 +0700

    fixed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Red-Bora-Portfolio/",
+  // Remove the 'base' property for Vercel deployment
 })
-
